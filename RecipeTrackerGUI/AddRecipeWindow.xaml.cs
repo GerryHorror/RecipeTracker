7aacdeb05e257e399ed35456694230652c6602cc
GerryHorror <GerryHorror@users.noreply.github.com>
2024-06-25 11:05:18 +0200

    - Cleaned up `using` directives and adjusted order - Added `NewRecipe` property and `ingredients` field - Implemented event handlers for ingredient and recipe management - Introduced methods for adding ingredients and saving recipes - Fixed file formatting by adding newline at end of files

--- RecipeTrackerGUI/AddRecipeWindow.xaml.cs
+++ RecipeTrackerGUI/AddRecipeWindow.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace RecipeTrackerGUI
 {
@@ -19,9 +9,62 @@ namespace RecipeTrackerGUI
     /// </summary>
     public partial class AddRecipeWindow : Window
     {
+        public Recipe NewRecipe { get; private set; }
+        private List<Ingredient> ingredients;
+
         public AddRecipeWindow()
         {
             InitializeComponent();
+            ingredients = new List<Ingredient>();
+        }
+
+        private void AddIngredient_Click(object sender, RoutedEventArgs e)
+        {
+            AddIngredientWindow addIngredientWindow = new AddIngredientWindow();
+            if (addIngredientWindow.ShowDialog() == true)
+            {
+                ingredients.Add(addIngredientWindow.NewIngredient);
+                UpdateIngredientsList();
+            }
+        }
+
+        private void UpdateIngredientsList()
+        {
+            IngredientsListBox.ItemsSource = null;
+            IngredientsListBox.ItemsSource = ingredients;
+        }
+
+        private void SaveRecipe_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(RecipeNameTextBox.Text))
+            {
+                MessageBox.Show("Please enter a recipe name.", "Missing Information", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (ingredients.Count == 0)
+            {
+                MessageBox.Show("Please add at least one ingredient.", "Missing Information", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(StepsTextBox.Text))
+            {
+                MessageBox.Show("Please enter the recipe steps.", "Missing Information", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            List<string> steps = new List<string>(StepsTextBox.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries));
+
+            NewRecipe = new Recipe(RecipeNameTextBox.Text, ingredients, steps);
+            DialogResult = true;
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
         }
     }
 }

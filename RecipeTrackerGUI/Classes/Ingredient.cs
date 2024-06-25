fa84b127862fe1cd3a29383f7d12da1f91888ded
GerryHorror <GerryHorror@users.noreply.github.com>
2024-06-25 13:11:33 +0200

    - Added a new class `Ingredient` to the RecipeTrackerGUI.Classes namespace - The class includes properties for name, quantity, unit, calories, and food group - Two constructors: one parameterless and another that initialises all properties - Updated `RecipeTrackerGUI.csproj` file to include `Ingredient.cs` as a compile entry

---
+++ RecipeTrackerGUI/Classes/Ingredient.cs
@@ -0,0 +1,23 @@
+﻿namespace RecipeTrackerGUI.Classes
+{
+    public class Ingredient
+    {
+        public string ingName { get; set; }
+        public double ingQty { get; set; }
+        public string ingUnit { get; set; }
+        public int Calories { get; set; }
+        public string FoodGroup { get; set; }
+
+        public Ingredient()
+        { }
+
+        public Ingredient(string name, double qty, string unit, int calories, string foodGroup)
+        {
+            ingName = name;
+            ingQty = qty;
+            ingUnit = unit;
+            Calories = calories;
+            FoodGroup = foodGroup;
+        }
+    }
+}

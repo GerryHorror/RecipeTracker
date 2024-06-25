a6275dca3a669ee62b0d1c15b81773ab3c60c2e8
GerryHorror <GerryHorror@users.noreply.github.com>
2024-06-25 13:03:39 +0200

    - Added Recipe management and scaling features - Introduced RecipeTrackerGUI.Classes namespace - Updated RecipeTrackerGUI.csproj to include Recipe class - Created Recipe class with properties and methods - Implemented recipe scaling and management features within the Recipe class

---
+++ RecipeTrackerGUI/Classes/Recipe.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RecipeTrackerGUI.Classes
+{
+    public class Recipe
+    {
+        public string recipeName { get; set; }
+        public List<Ingredient> ingredients { get; set; }
+        public List<string> steps { get; set; }
+        public List<double> originalQty { get; set; }
+        public List<string> originalUnits { get; set; }
+
+        public Recipe()
+        {
+            recipeName = string.Empty;
+            ingredients = new List<Ingredient>();
+            steps = new List<string>();
+            originalQty = new List<double>();
+            originalUnits = new List<string>();
+        }
+
+        public Recipe(string name, List<Ingredient> ing, List<string> steps)
+        {
+            recipeName = name;
+            ingredients = ing;
+            this.steps = steps;
+            originalQty = ing.Select(ingredient => ingredient.ingQty).ToList();
+            originalUnits = ing.Select(ingredient => ingredient.ingUnit).ToList();
+        }
+
+        public void AddIngredient(string name, double qty, string unit, int calories, string foodGroup)
+        {
+            Ingredient newIngredient = new Ingredient(name, qty, unit, calories, foodGroup);
+            ingredients.Add(newIngredient);
+            originalQty.Add(qty);
+            originalUnits.Add(unit);
+        }
+
+        public void AddStep(string step)
+        {
+            steps.Add(step);
+        }
+
+        public bool ScaleRecipe(double factor)
+        {
+            if (factor != 0.5 && factor != 2 && factor != 3)
+            {
+                return false;
+            }
+            for (var i = 0; i < ingredients.Count; i++)
+            {
+                (double scaledQty, string newUnit) = RecipeOperations.ConvertUnit(ingredients[i].ingUnit, ingredients[i].ingQty, factor);
+                ingredients[i].ingQty = scaledQty;
+                ingredients[i].ingUnit = newUnit;
+            }
+            return true;
+        }
+
+        public void ResetQuantity()
+        {
+            for (var i = 0; i < ingredients.Count; i++)
+            {
+                ingredients[i].ingQty = originalQty[i];
+                ingredients[i].ingUnit = originalUnits[i];
+            }
+        }
+
+        public void ClearRecipe()
+        {
+            ingredients.Clear();
+            steps.Clear();
+            originalQty.Clear();
+            originalUnits.Clear();
+        }
+    }
+}
